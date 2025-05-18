b589f2bf2c982ce1f92c392738942c0f658e512d
mayanklakhani98 <monmayank@gmail.com>
2025-05-18 12:48:19 +0530

    changes add a console

--- test/src/counter.jsx
+++ test/src/counter.jsx
@@ -15,6 +15,7 @@ const Counter = () => {
             <button onClick={() => handleOperation(-1)}>-</button>
             <h2>count is : {count}</h2>
             <button onClick={() => handleOperation(+1)}>+</button>
+        {console.log("first commit")}
         </div>  
     )
 }
